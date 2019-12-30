4cd3b930dff5d677c090f5bd64a1e1cda2849356
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2019-12-30 19:23:22 +0100

    Menu3
    Mostrar data dependiendo la opcion escogida

---
+++ js/data_menu.js
@@ -0,0 +1,12 @@
+var hide = $('.hide');
+var show = $('.show');
+$(document).ready(function(){
+    $("#alexa_but").on('click', function(event){
+        $('#alexa').addClass('show').removeClass('hide');
+        $('#whats').addClass('hide').removeClass('show');
+    });
+    $("#whats_but").on('click', function(event){
+        $('#whats').addClass('show').removeClass('hide');
+        $('#alexa').addClass('hide').removeClass('show');
+    });
+  });
