1914e157e01e70c7520c9d5beeb54a2db436c99b
santi <47889917+ezequielucho@users.noreply.github.com>
2020-01-01 13:08:56 +0100

    Style

--- js/add_project.js
+++ js/add_project.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $("#add_project").on('click', function(event){
         console.log("funciona");
         var div = document.createElement('div');
+        var li = '<li id="add_project"><i class="material-icons">dashboard</i><span>Prueba</span></li id="add_project">';
+        $('#menu').append(li);
         $('#container').append(div);
     });
   });
