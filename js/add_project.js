6116c9291b86d8516011fdcd19a4c96dbe760666
santi <47889917+ezequielucho@users.noreply.github.com>
2020-01-03 12:53:03 +0100

    Update add_project.js

--- js/add_project.js
+++ js/add_project.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#add_project").on('click', function(event){
         console.log("funciona");
-        var div = document.createElement('div');
+        var div = '<div class="hide"></div>';
         var li = '<li id="add_project"><i class="material-icons">dashboard</i><span>Prueba</span></li id="add_project">';
         $('#menu').append(li);
         $('#container').append(div);
