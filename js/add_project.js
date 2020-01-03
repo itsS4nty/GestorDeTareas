56d456d7c3585cb7200586eea33ca4fa3133d5dd
santi <47889917+ezequielucho@users.noreply.github.com>
2020-01-03 13:04:17 +0100

    Update add_project.js

--- js/add_project.js
+++ js/add_project.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     $("#add_project").on('click', function(event){
         console.log("funciona");
-        var div = '<div class="hide"></div>';
-        var li = '<li id="add_project"><i class="material-icons">dashboard</i><span>Prueba</span></li id="add_project">';
+        var new_proj = prompt("Introduce el nombre del proyecto");
+        var div_id = new_proj.replace(" ", "").toLowerCase();
+        var li_id = new_proj.replace(" ", "").toLowerCase() + "_" + "but"; 
+        var div = `<div id="${div_id}" class="hide"></div>`;
+        var li = `<li id="${li_id}"><i class="material-icons">dashboard</i><span>${new_proj}</span></li id="add_project">`;
         $('#menu').append(li);
         $('#container').append(div);
     });
